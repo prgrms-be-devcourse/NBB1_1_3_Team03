824c17274478fa4cdfa2b841204f6a1d5d369039
jhk01007 <jhk001007@naver.com>
2024-11-01 17:21:32 +0900

    migration: CustomHighlightConverter 코틀린 마이그레이션
    - 별도로 수정한 부분 없음

--- src/main/java/com/sscanner/team/global/configure/CustomHighlightConverter.kt
+++ src/main/java/com/sscanner/team/global/configure/CustomHighlightConverter.kt
@@ -1,24 +1,21 @@
-package com.sscanner.team.global.configure;
+package com.sscanner.team.global.configure
 
-import ch.qos.logback.classic.spi.ILoggingEvent;
-import ch.qos.logback.core.pattern.color.ANSIConstants;
-import ch.qos.logback.core.pattern.color.ForegroundCompositeConverterBase;
+import ch.qos.logback.classic.spi.ILoggingEvent
+import ch.qos.logback.core.pattern.color.ANSIConstants
+import ch.qos.logback.core.pattern.color.ForegroundCompositeConverterBase
 
 /**
  * logback-spring 로그 레벨별 색상 지정 Custom Converter Class
  */
-public class CustomHighlightConverter extends ForegroundCompositeConverterBase<ILoggingEvent> {
-
-    @Override
-    protected String getForegroundColorCode(ILoggingEvent event) {
-        return switch (event.getLevel().toString()) {
-            case "ERROR" -> ANSIConstants.BOLD + ANSIConstants.RED_FG;
-            case "WARN" -> ANSIConstants.BOLD + ANSIConstants.YELLOW_FG;
-            case "INFO" -> ANSIConstants.BOLD + ANSIConstants.GREEN_FG;
-            case "DEBUG" -> ANSIConstants.BOLD + ANSIConstants.BLUE_FG;
-            case "TRACE" -> ANSIConstants.BLUE_FG;
-            default -> ANSIConstants.DEFAULT_FG;
-        };
+class CustomHighlightConverter : ForegroundCompositeConverterBase<ILoggingEvent>() {
+    override fun getForegroundColorCode(event: ILoggingEvent): String {
+        return when (event.level.toString()) {
+            "ERROR" -> ANSIConstants.BOLD + ANSIConstants.RED_FG
+            "WARN" -> ANSIConstants.BOLD + ANSIConstants.YELLOW_FG
+            "INFO" -> ANSIConstants.BOLD + ANSIConstants.GREEN_FG
+            "DEBUG" -> ANSIConstants.BOLD + ANSIConstants.BLUE_FG
+            "TRACE" -> ANSIConstants.BLUE_FG
+            else -> ANSIConstants.DEFAULT_FG
+        }
     }
-
 }
