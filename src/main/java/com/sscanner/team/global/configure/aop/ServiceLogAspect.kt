6d2cf4989fbbbf67e5c7a4025002da209e641dd6
jhk01007 <jhk001007@naver.com>
2024-11-01 17:24:42 +0900

    migration: ServiceLogAspect 코틀린 마이그레이션
    - @Slf4j를 삭제하고 LoggerFactory를 통해 Logger를 받아와서 사용하는 것으로 수정함

--- src/main/java/com/sscanner/team/global/configure/aop/ServiceLogAspect.kt
+++ src/main/java/com/sscanner/team/global/configure/aop/ServiceLogAspect.kt
@@ -1,49 +1,52 @@
-package com.sscanner.team.global.configure.aop;
+package com.sscanner.team.global.configure.aop
 
-import com.sscanner.team.global.exception.BadRequestException;
-import com.sscanner.team.global.exception.DuplicateException;
-import jakarta.servlet.http.HttpServletRequest;
-import lombok.extern.slf4j.Slf4j;
-import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.*;
-import org.aspectj.lang.reflect.MethodSignature;
-import org.slf4j.MDC;
-import org.springframework.stereotype.Component;
-import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.context.request.RequestContextHolder;
-import org.springframework.web.context.request.ServletRequestAttributes;
-
-import java.util.Arrays;
-import java.util.UUID;
+import lombok.extern.slf4j.Slf4j
+import org.aspectj.lang.JoinPoint
+import org.aspectj.lang.annotation.*
+import org.aspectj.lang.reflect.MethodSignature
+import org.slf4j.LoggerFactory
+import org.springframework.stereotype.Component
 
 @Component
 @Aspect
-@Slf4j
-public class ServiceLogAspect {
-
-    private long startTime;
+class ServiceLogAspect {
+    private var startTime: Long = 0
+    private val log = LoggerFactory.getLogger(this.javaClass)!!
 
     // service의 모든 메서드에 대해 적용
     @Pointcut("execution(* com.sscanner.team..service.*.*(..))")
-    public void pointCut() {}
+    fun pointCut() {
+    }
 
     // 메서드 호출 전 로그 남기기
     @Before("pointCut()")
-    public void logBefore(JoinPoint joinPoint) {
-        startTime = System.currentTimeMillis();
-        log.info("Entering Service: Method={} with Args={}", ((MethodSignature) joinPoint.getSignature()).getMethod().getName(), Arrays.toString(joinPoint.getArgs()));
+    fun logBefore(joinPoint: JoinPoint) {
+        startTime = System.currentTimeMillis()
+        log.info(
+            "Entering Service: Method={} with Args={}",
+            (joinPoint.signature as MethodSignature).method.name,
+            joinPoint.args.contentToString()
+        )
     }
 
     // 메서드 호출 후 정상적으로 반환된 경우 로그 남기기
     @AfterReturning(pointcut = "pointCut()", returning = "result")
-    public void logAfterReturning(JoinPoint joinPoint, Object result) {
-        log.info("Exiting Service: Method={} with Return={}", ((MethodSignature) joinPoint.getSignature()).getMethod().getName(), result);
+    fun logAfterReturning(joinPoint: JoinPoint, result: Any?) {
+        log.info(
+            "Exiting Service: Method={} with Return={}",
+            (joinPoint.signature as MethodSignature).method.name,
+            result
+        )
     }
 
     // 완전히 종료된후 메서드 실행시간 측정하기
     @After("pointCut()")
-    public void logAfter(JoinPoint joinPoint) {
-        long executionTime = System.currentTimeMillis() - startTime;
-        log.info("Method={} ExecutionTime={}ms", ((MethodSignature) joinPoint.getSignature()).getMethod().getName(), executionTime);
+    fun logAfter(joinPoint: JoinPoint) {
+        val executionTime = System.currentTimeMillis() - startTime
+        log.info(
+            "Method={} ExecutionTime={}ms",
+            (joinPoint.signature as MethodSignature).method.name,
+            executionTime
+        )
     }
 }
