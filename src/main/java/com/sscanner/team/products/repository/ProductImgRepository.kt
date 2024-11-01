145e42648c223c37106a2f7d0be2b8f379a0576a
ryu <jeungwon28@gmail.com>
2024-11-01 15:31:20 +0900

    migration: Repository 변환

--- src/main/java/com/sscanner/team/products/repository/ProductImgRepository.kt
+++ src/main/java/com/sscanner/team/products/repository/ProductImgRepository.kt
@@ -1,11 +1,9 @@
-package com.sscanner.team.products.repository;
+package com.sscanner.team.products.repository
 
-import com.sscanner.team.products.entity.ProductImg;
-import org.springframework.data.jpa.repository.JpaRepository;
+import com.sscanner.team.products.entity.ProductImg
+import org.springframework.data.jpa.repository.JpaRepository
 
-import java.util.List;
-
-public interface ProductImgRepository extends JpaRepository<ProductImg, Long> {
-    List<ProductImg> findAllByProductId(Long productId);
-    List<ProductImg> findAllByProductIdIn(List<Long> productIds);
+interface ProductImgRepository : JpaRepository<ProductImg, Long> {
+    fun findAllByProductId(productId: Long): List<ProductImg>
+    fun findAllByProductIdIn(productIds: List<Long>): List<ProductImg>
 }
