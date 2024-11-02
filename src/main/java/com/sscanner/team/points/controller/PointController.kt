28d242edb23bd1ad3f0aa8eace6be73a418ceae9
ryu <jeungwon28@gmail.com>
2024-11-02 23:43:23 +0900

    refactor: controller migration

--- src/main/java/com/sscanner/team/points/controller/PointController.kt
+++ src/main/java/com/sscanner/team/points/controller/PointController.kt
@@ -1,29 +1,23 @@
-package com.sscanner.team.points.controller;
+package com.sscanner.team.points.controller
 
-import com.sscanner.team.global.common.response.ApiResponse;
-import com.sscanner.team.points.dto.requestdto.PointRequestDto;
-import com.sscanner.team.points.dto.responsedto.PointWithUserIdResponseDto;
-import com.sscanner.team.points.service.PointService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
-
+import com.sscanner.team.global.common.response.ApiResponse
+import com.sscanner.team.points.dto.requestdto.PointRequestDto
+import com.sscanner.team.points.dto.responsedto.PointWithUserIdResponseDto
+import com.sscanner.team.points.service.PointService
+import org.springframework.web.bind.annotation.*
 
 @RestController
-@RequiredArgsConstructor
 @RequestMapping("/api/points")
-public class PointController {
-
-    private final PointService pointService;
-
+class PointController(private val pointService: PointService) {
     @GetMapping("/{userId}")
-    public ApiResponse<PointWithUserIdResponseDto> getUserPoints(@PathVariable String userId) {
-        PointWithUserIdResponseDto pointWithUserIdResponseDto = pointService.getCachedPoint(userId);
-        return ApiResponse.ok(200, pointWithUserIdResponseDto, "사용자 포인트 조회 성공");
+    fun getUserPoints(@PathVariable userId: String): ApiResponse<PointWithUserIdResponseDto?> {
+        val pointWithUserIdResponseDto = pointService.getCachedPoint(userId)
+        return ApiResponse.ok(200, pointWithUserIdResponseDto, "사용자 포인트 조회 성공")
     }
 
     @PostMapping("/add")
-    public ApiResponse<PointWithUserIdResponseDto> addUserPoints(@RequestBody PointRequestDto pointRequestDto) {
-        PointWithUserIdResponseDto response = pointService.addPoint(pointRequestDto);
-        return ApiResponse.ok(201, response, "포인트가 성공적으로 추가되었습니다.");
+    fun addUserPoints(@RequestBody pointRequestDto: PointRequestDto): ApiResponse<PointWithUserIdResponseDto?> {
+        val response = pointService.addPoint(pointRequestDto)
+        return ApiResponse.ok(201, response, "포인트가 성공적으로 추가되었습니다.")
     }
 }
