c35d2aa0c4a8f0217db2d2086eacedeabfba4659
ryu <jeungwon28@gmail.com>
2024-11-03 22:16:33 +0900

    migration: barcode controller migration

--- src/main/java/com/sscanner/team/barcode/controller/BarcodeController.kt
+++ src/main/java/com/sscanner/team/barcode/controller/BarcodeController.kt
@@ -1,26 +1,19 @@
-package com.sscanner.team.barcode.controller;
+package com.sscanner.team.barcode.controller
 
-import com.sscanner.team.barcode.responsedto.BarcodeResponseDto;
-import com.sscanner.team.barcode.service.BarcodeService;
-import com.sscanner.team.global.common.response.ApiResponse;
-import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
+import com.sscanner.team.barcode.responsedto.BarcodeResponseDto
+import com.sscanner.team.barcode.service.BarcodeService
+import com.sscanner.team.global.common.response.ApiResponse
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequiredArgsConstructor
 @RequestMapping("/api/barcodes")
-public class BarcodeController {
-
-    private final BarcodeService barcodeService;
-
+class BarcodeController(private val barcodeService: BarcodeService) {
     @GetMapping("/{userId}")
-    public ApiResponse<List<BarcodeResponseDto>> getUserBarcodes(@PathVariable String userId) {
-        List<BarcodeResponseDto> barcodes = barcodeService.findBarcodesByUserId(userId);
-        return ApiResponse.ok(200, barcodes, "바코드 목록 조회 성공");
+    fun getUserBarcodes(@PathVariable userId: String): ApiResponse<List<BarcodeResponseDto>> {
+        val barcodes = barcodeService.findBarcodesByUserId(userId)
+        return ApiResponse.ok(200, barcodes, "바코드 목록 조회 성공")
     }
 }
