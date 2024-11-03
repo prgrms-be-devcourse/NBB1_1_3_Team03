f168cedbe43515299018eaa948ecd4ff2aee541a
ryu <jeungwon28@gmail.com>
2024-11-03 22:15:02 +0900

    migration: barcode constants migration

--- src/main/java/com/sscanner/team/barcode/common/BarcodeConstants.kt
+++ src/main/java/com/sscanner/team/barcode/common/BarcodeConstants.kt
@@ -1,8 +1,8 @@
-package com.sscanner.team.barcode.common;
+package com.sscanner.team.barcode.common
 
-public class BarcodeConstants {
-    public static final String BARCODE_TEXT_TEMPLATE = "ProductID: %d, UserID: %s";
-    public static final String IMAGE_FORMAT = "png";
-    public static final Integer BARCODE_WIDTH = 300;
-    public static final Integer BARCODE_HEIGHT = 100;
+object BarcodeConstants {
+    const val BARCODE_TEXT_TEMPLATE: String = "ProductID: %d, UserID: %s"
+    const val IMAGE_FORMAT: String = "png"
+    const val BARCODE_WIDTH: Int = 300
+    const val BARCODE_HEIGHT: Int = 100
 }
