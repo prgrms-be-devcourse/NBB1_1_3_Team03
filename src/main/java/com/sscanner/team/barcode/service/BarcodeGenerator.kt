57189a579b6374e0895c8ef30f4dc8e2a2ee46b6
ryu <jeungwon28@gmail.com>
2024-11-03 22:16:15 +0900

    migration: barcode generator migration

--- src/main/java/com/sscanner/team/barcode/service/BarcodeGenerator.kt
+++ src/main/java/com/sscanner/team/barcode/service/BarcodeGenerator.kt
@@ -1,30 +1,33 @@
-package com.sscanner.team.barcode.service;
+package com.sscanner.team.barcode.service
 
-
-import com.google.zxing.BarcodeFormat;
-import com.google.zxing.MultiFormatWriter;
-import com.google.zxing.WriterException;
-import com.google.zxing.client.j2se.MatrixToImageWriter;
-import com.google.zxing.common.BitMatrix;
-import org.springframework.stereotype.Component;
-
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.util.Base64;
-
-import static com.sscanner.team.barcode.common.BarcodeConstants.*;
+import com.google.zxing.BarcodeFormat
+import com.google.zxing.MultiFormatWriter
+import com.google.zxing.WriterException
+import com.google.zxing.client.j2se.MatrixToImageWriter
+import com.sscanner.team.barcode.common.BarcodeConstants
+import org.springframework.stereotype.Component
+import java.io.ByteArrayOutputStream
+import java.io.IOException
+import java.util.*
 
 @Component
-public class BarcodeGenerator {
-
-    public String generateBarcodeImage(String barcodeText) {
+class BarcodeGenerator {
+    fun generateBarcodeImage(barcodeText: String): String {
         try {
-            BitMatrix bitMatrix = new MultiFormatWriter().encode(barcodeText, BarcodeFormat.CODE_128, BARCODE_WIDTH, BARCODE_HEIGHT);
-            ByteArrayOutputStream baos = new ByteArrayOutputStream();
-            MatrixToImageWriter.writeToStream(bitMatrix, IMAGE_FORMAT, baos);
-            return Base64.getEncoder().encodeToString(baos.toByteArray());
-        } catch (WriterException | IOException e) {
-            throw new RuntimeException("Failed to generate barcode image", e); // 구체적인 예외 처리
+            val bitMatrix = MultiFormatWriter().encode(
+                barcodeText,
+                BarcodeFormat.CODE_128,
+                BarcodeConstants.BARCODE_WIDTH,
+                BarcodeConstants.BARCODE_HEIGHT
+            )
+            ByteArrayOutputStream().use { baos ->
+                MatrixToImageWriter.writeToStream(bitMatrix, BarcodeConstants.IMAGE_FORMAT, baos)
+                return Base64.getEncoder().encodeToString(baos.toByteArray())
+            }
+        } catch (e: WriterException) {
+            throw RuntimeException("Failed to generate barcode image due to writing error", e)
+        } catch (e: IOException) {
+            throw RuntimeException("Failed to generate barcode image due to I/O error", e)
         }
     }
 }
