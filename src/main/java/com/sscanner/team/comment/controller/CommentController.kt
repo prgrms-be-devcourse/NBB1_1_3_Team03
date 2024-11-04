8d34c50a15bd978d30d4be7e659d21ba3ac65558
ihun-il <hunil9978@gmail.com>
2024-11-04 14:16:21 +0900

    migration: Comment Controller 마이그레이션

--- src/main/java/com/sscanner/team/comment/controller/CommentController.kt
+++ src/main/java/com/sscanner/team/comment/controller/CommentController.kt
@@ -1,47 +1,42 @@
-package com.sscanner.team.comment.controller;
+package com.sscanner.team.comment.controller
 
-import com.sscanner.team.comment.requestdto.CommentCreateRequestDTO;
-import com.sscanner.team.comment.responsedto.CommentResponseDTO;
-import com.sscanner.team.comment.service.CommentService;
-import com.sscanner.team.global.common.response.ApiResponse;
-import jakarta.validation.Valid;
-import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
+import com.sscanner.team.comment.requestdto.CommentCreateRequestDTO
+import com.sscanner.team.comment.responsedto.CommentResponseDTO
+import com.sscanner.team.comment.service.CommentService
+import com.sscanner.team.global.common.response.ApiResponse
+import jakarta.validation.Valid
+import org.springframework.web.bind.annotation.*
 
 @RestController
-@RequiredArgsConstructor
 @RequestMapping("/api/comments")
-public class CommentController {
-
-    private final CommentService commentService;
-
+class CommentController(
+    private val commentService: CommentService
+) {
     @PostMapping
-    public ApiResponse<CommentResponseDTO> createComment(@Valid @RequestBody CommentCreateRequestDTO commentCreateRequestDTO) {
-        CommentResponseDTO commentInfo = commentService.saveComment(commentCreateRequestDTO);
+    fun createComment(@Valid @RequestBody commentCreateRequestDTO: CommentCreateRequestDTO): ApiResponse<CommentResponseDTO> {
+        val commentInfo = commentService.saveComment(commentCreateRequestDTO)
 
-        return ApiResponse.ok(201, commentInfo, "댓글 생성 완료!!");
+        return ApiResponse.ok(201, commentInfo, "댓글 생성 완료!!")
     }
 
     @DeleteMapping("/{commmentId}")
-    public ApiResponse<Void> deleteComment(@PathVariable Long commmentId) {
-        commentService.deleteComment(commmentId);
+    fun deleteComment(@PathVariable commmentId: Long): ApiResponse<Any> {
+        commentService.deleteComment(commmentId)
 
-        return ApiResponse.ok(200, null, "댓글 삭제 완료!!");
+        return ApiResponse.ok(200, null, "댓글 삭제 완료!!")
     }
 
     @GetMapping("/{boardId}")
-    public ApiResponse<List<CommentResponseDTO>> getComments(@PathVariable Long boardId) {
-        List<CommentResponseDTO> comments = commentService.getComments(boardId);
+    fun getComments(@PathVariable boardId: Long): ApiResponse<List<CommentResponseDTO>> {
+        val comments = commentService.getComments(boardId)
 
-        return ApiResponse.ok(200, comments, "댓글 조회 완료!!");
+        return ApiResponse.ok(200, comments, "댓글 조회 완료!!")
     }
 
     @DeleteMapping("/all/{boardId}")
-    public ApiResponse<Void> deleteAllComments(@PathVariable Long boardId) {
-        commentService.deleteAll(boardId);
+    fun deleteAllComments(@PathVariable boardId: Long): ApiResponse<Void> {
+        commentService.deleteAll(boardId)
 
-        return ApiResponse.ok(200, null, "모든 댓글 삭제 완료!!");
+        return ApiResponse.ok(200, null, "모든 댓글 삭제 완료!!")
     }
 }
