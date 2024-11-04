c1f8d73bb4289d69c3a20ff31b20631a5588bf05
ihun-il <hunil9978@gmail.com>
2024-11-04 12:25:49 +0900

    migration: Admin 관련 Service 마이그레이션

--- src/main/java/com/sscanner/team/admin/service/AdminService.kt
+++ src/main/java/com/sscanner/team/admin/service/AdminService.kt
@@ -1,17 +1,20 @@
-package com.sscanner.team.admin.service;
+package com.sscanner.team.admin.service
 
-import com.sscanner.team.admin.requestdto.AdminBoardRequestDTO;
-import com.sscanner.team.admin.responsedto.AdminBoardListResponseDTO;
-import com.sscanner.team.admin.responsedto.AdminEctBoardResponseDTO;
-import com.sscanner.team.admin.responsedto.AdminModifyBoardResponseDTO;
-import com.sscanner.team.board.type.ApprovalStatus;
-import com.sscanner.team.board.type.BoardCategory;
-import com.sscanner.team.trashcan.type.TrashCategory;
+import com.sscanner.team.admin.requestdto.AdminBoardRequestDTO
+import com.sscanner.team.admin.responsedto.AdminBoardListResponseDTO
+import com.sscanner.team.admin.responsedto.AdminEctBoardResponseDTO
+import com.sscanner.team.admin.responsedto.AdminModifyBoardResponseDTO
+import com.sscanner.team.board.type.ApprovalStatus
+import com.sscanner.team.board.type.BoardCategory
+import com.sscanner.team.trashcan.type.TrashCategory
 
-public interface AdminService {
-    AdminBoardListResponseDTO getBoards(ApprovalStatus approvalStatus, TrashCategory trashCategory,
-                                        BoardCategory boardCategory, Integer page, Integer size);
-    AdminModifyBoardResponseDTO getModifyBoard(Long boardId);
-    AdminEctBoardResponseDTO getEctBoard(Long boardId);
-    void reflectBoard(Long boardId, AdminBoardRequestDTO adminBoardRequestDTO);
+interface AdminService {
+    fun getBoards(
+        approvalStatus: ApprovalStatus, trashCategory: TrashCategory,
+        boardCategory: BoardCategory, page: Int, size: Int
+    ): AdminBoardListResponseDTO
+
+    fun getModifyBoard(boardId: Long): AdminModifyBoardResponseDTO
+    fun getEctBoard(boardId: Long): AdminEctBoardResponseDTO
+    fun reflectBoard(boardId: Long, adminBoardRequestDTO: AdminBoardRequestDTO)
 }
--- src/main/java/com/sscanner/team/admin/service/AdminServiceImpl.kt
+++ src/main/java/com/sscanner/team/admin/service/AdminServiceImpl.kt
@@ -1,146 +1,131 @@
-package com.sscanner.team.admin.service;
+package com.sscanner.team.admin.service
 
-import com.sscanner.team.admin.requestdto.AdminBoardRequestDTO;
-import com.sscanner.team.admin.responsedto.AdminBoardInfoResponseDTO;
-import com.sscanner.team.admin.responsedto.AdminBoardListResponseDTO;
-import com.sscanner.team.admin.responsedto.AdminEctBoardResponseDTO;
-import com.sscanner.team.admin.responsedto.AdminModifyBoardResponseDTO;
-import com.sscanner.team.board.entity.Board;
-import com.sscanner.team.board.entity.BoardImg;
-import com.sscanner.team.board.service.BoardImgService;
-import com.sscanner.team.board.service.BoardService;
-import com.sscanner.team.board.type.ApprovalStatus;
-import com.sscanner.team.board.type.BoardCategory;
-import com.sscanner.team.global.exception.BadRequestException;
-import com.sscanner.team.trashcan.entity.Trashcan;
-import com.sscanner.team.trashcan.entity.TrashcanImg;
-import com.sscanner.team.trashcan.service.TrashcanImgService;
-import com.sscanner.team.trashcan.service.TrashcanService;
-import com.sscanner.team.trashcan.type.TrashCategory;
-import com.sscanner.team.trashcan.type.TrashcanStatus;
-import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
-
-import static com.sscanner.team.global.exception.ExceptionCode.MISMATCH_BOARD_TYPE;
+import com.sscanner.team.admin.requestdto.AdminBoardRequestDTO
+import com.sscanner.team.admin.responsedto.AdminBoardListResponseDTO
+import com.sscanner.team.admin.responsedto.AdminEctBoardResponseDTO
+import com.sscanner.team.admin.responsedto.AdminModifyBoardResponseDTO
+import com.sscanner.team.board.entity.Board
+import com.sscanner.team.board.entity.BoardImg
+import com.sscanner.team.board.service.BoardImgService
+import com.sscanner.team.board.service.BoardService
+import com.sscanner.team.board.type.ApprovalStatus
+import com.sscanner.team.board.type.BoardCategory
+import com.sscanner.team.global.exception.BadRequestException
+import com.sscanner.team.global.exception.ExceptionCode
+import com.sscanner.team.trashcan.entity.Trashcan
+import com.sscanner.team.trashcan.service.TrashcanImgService
+import com.sscanner.team.trashcan.service.TrashcanService
+import com.sscanner.team.trashcan.type.TrashCategory
+import com.sscanner.team.trashcan.type.TrashcanStatus
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
 @Service
-@RequiredArgsConstructor
 @Transactional(readOnly = true)
-public class AdminServiceImpl implements AdminService {
+class AdminServiceImpl(
+    private val boardService: BoardService,
+    private val boardImgService: BoardImgService,
+    private val trashcanService: TrashcanService,
+    private val trashcanImgService: TrashcanImgService
+) : AdminService {
 
-    private final BoardService boardService;
-    private final BoardImgService boardImgService;
-    private final TrashcanService trashcanService;
-    private final TrashcanImgService trashcanImgService;
+    override fun getBoards(
+        approvalStatus: ApprovalStatus, trashCategory: TrashCategory,
+        boardCategory: BoardCategory, page: Int, size: Int
+    ): AdminBoardListResponseDTO {
+        val pageBoards =
+            boardService.getBoardsForAdmin(approvalStatus, trashCategory, boardCategory, page, size)
 
-    @Override
-    public AdminBoardListResponseDTO getBoards(ApprovalStatus approvalStatus, TrashCategory trashCategory,
-                                               BoardCategory boardCategory, Integer page, Integer size) {
-        Page<AdminBoardInfoResponseDTO> pageBoards =
-                boardService.getBoardsForAdmin(approvalStatus, trashCategory, boardCategory, page, size);
-
-        return AdminBoardListResponseDTO.of(approvalStatus, trashCategory, boardCategory, pageBoards);
+        return AdminBoardListResponseDTO.of(approvalStatus, trashCategory, boardCategory, pageBoards)
     }
 
-    @Override
-    public AdminModifyBoardResponseDTO getModifyBoard(Long boardId) {
-        Board board = boardService.getBoard(boardId);
+    override fun getModifyBoard(boardId: Long): AdminModifyBoardResponseDTO {
+        val board = boardService.getBoard(boardId)
 
-        isModifyBoard(board);
+        isModifyBoard(board)
 
-        List<BoardImg> boardImgs = boardImgService.getBoardImgs(boardId);
+        val boardImgs = boardImgService.getBoardImgs(boardId)
 
-        Trashcan trashcan = trashcanService.getTrashcanById(board.getTrashcanId());
-        TrashcanImg trashcanImg = trashcanImgService.getTrashcanImg(board.getTrashcanId());
+        val trashcan = trashcanService.getTrashcanById(board.trashcanId)
+        val trashcanImg = trashcanImgService.getTrashcanImg(board.trashcanId)
 
-        return AdminModifyBoardResponseDTO.of(trashcan, trashcanImg, board, boardImgs);
+        return AdminModifyBoardResponseDTO.of(trashcan, trashcanImg, board, boardImgs)
     }
 
-    @Override
-    public AdminEctBoardResponseDTO getEctBoard(Long boardId) {
-        Board board = boardService.getBoard(boardId);
+    override fun getEctBoard(boardId: Long): AdminEctBoardResponseDTO {
+        val board = boardService.getBoard(boardId)
 
-        isNotModifyBoard(board);
+        isNotModifyBoard(board)
 
-        List<BoardImg> boardImgs = boardImgService.getBoardImgs(boardId);
+        val boardImgs: List<BoardImg?> = boardImgService.getBoardImgs(boardId)
 
-        return AdminEctBoardResponseDTO.of(board, boardImgs);
+        return AdminEctBoardResponseDTO.of(board, boardImgs)
     }
 
     @Transactional
-    @Override
-    public void reflectBoard(Long boardId, AdminBoardRequestDTO adminBoardRequestDTO) {
-        Board board = boardService.getBoard(boardId);
+    override fun reflectBoard(boardId: Long, adminBoardRequestDTO: AdminBoardRequestDTO) {
+        val board = boardService.getBoard(boardId)
 
-        ApprovalStatus approvalStatus = adminBoardRequestDTO.approvalStatus();
-        String chosenImgUrl = adminBoardRequestDTO.chosenImgUrl();
+        val approvalStatus = adminBoardRequestDTO.approvalStatus
+        val chosenImgUrl = adminBoardRequestDTO.chosenImgUrl
 
-        boardImgService.checkExistImgUrl(boardId, chosenImgUrl);
+        boardImgService.checkExistImgUrl(boardId, chosenImgUrl)
 
-        if(approvalStatus.equals(ApprovalStatus.APPROVED)) {
-            processBoardApproval(board, chosenImgUrl);
+        if (approvalStatus == ApprovalStatus.APPROVED) {
+            processBoardApproval(board, chosenImgUrl)
         }
 
-        board.changeApprovalStatus(approvalStatus);
+        board.changeApprovalStatus(approvalStatus)
     }
 
-    private void processBoardApproval(Board board, String chosenImgUrl) {
-        BoardCategory boardCategory = board.getBoardCategory();
+    private fun processBoardApproval(board: Board, chosenImgUrl: String) {
+        val boardCategory = board.boardCategory
 
-        switch (boardCategory) {
-            case MODIFY:
-                approveModifyBoard(board.getTrashcanId(), board.getUpdatedTrashcanStatus(), chosenImgUrl);
-                break;
-            case REMOVE:
-                approveRemoveBoard(board.getTrashcanId());
-                break;
-            case ADD:
-                approveAddBoard(board, chosenImgUrl);
-                break;
-            default:
-                throw new BadRequestException(MISMATCH_BOARD_TYPE);
+        when (boardCategory) {
+            BoardCategory.MODIFY -> approveModifyBoard(board.trashcanId!!, board.updatedTrashcanStatus, chosenImgUrl)
+            BoardCategory.REMOVE -> approveRemoveBoard(board.trashcanId!!)
+            BoardCategory.ADD -> approveAddBoard(board, chosenImgUrl)
+            else -> throw BadRequestException(ExceptionCode.MISMATCH_BOARD_TYPE)
         }
     }
 
-    private void approveModifyBoard(Long trashcanId,
-                                   TrashcanStatus updatedTrashcanStatus,
-                                   String chosenImgUrl) {
-        Trashcan trashcan = trashcanService.getTrashcanById(trashcanId);
-        TrashcanImg trashcanImg = trashcanImgService.getTrashcanImg(trashcanId);
+    private fun approveModifyBoard(
+        trashcanId: Long,
+        updatedTrashcanStatus: TrashcanStatus,
+        chosenImgUrl: String
+    ) {
+        val trashcan = trashcanService.getTrashcanById(trashcanId)
+        val trashcanImg = trashcanImgService.getTrashcanImg(trashcanId)
 
-        trashcan.changeTrashcanStatus(updatedTrashcanStatus);
-        trashcanImg.changeImgUrl(chosenImgUrl);
+        trashcan.changeTrashcanStatus(updatedTrashcanStatus)
+        trashcanImg.changeImgUrl(chosenImgUrl)
     }
 
-    private void approveRemoveBoard(Long trashcanId) {
-        Trashcan trashcan = trashcanService.getTrashcanById(trashcanId);
+    private fun approveRemoveBoard(trashcanId: Long) {
+        val trashcan = trashcanService.getTrashcanById(trashcanId)
 
-        trashcanService.deleteTrashcanInfo(trashcan.getId());
-        trashcanImgService.deleteTrashcanImg(trashcan.getId());
+        trashcanService.deleteTrashcanInfo(trashcan.id)
+        trashcanImgService.deleteTrashcanImg(trashcan.id)
     }
 
-    private void approveAddBoard(Board board, String chosenImgUrl) {
-        Trashcan trashcan = board.toEntityTrashcan();
+    private fun approveAddBoard(board: Board, chosenImgUrl: String) {
+        val trashcan: Trashcan = board.toEntityTrashcan()
 
-        trashcanService.saveTrashcan(trashcan);
-        trashcanImgService.saveTrashcanImg(trashcan.getId(), chosenImgUrl);
+        trashcanService.saveTrashcan(trashcan)
+        trashcanImgService.saveTrashcanImg(trashcan.id, chosenImgUrl)
 
-        board.changeTrashcanId(trashcan.getId());
+        board.changeTrashcanId(trashcan.id!!)
     }
 
-    private void isModifyBoard(Board board) {
-        if(!board.getBoardCategory().equals(BoardCategory.MODIFY)) {
-            throw new BadRequestException(MISMATCH_BOARD_TYPE);
+    private fun isModifyBoard(board: Board) {
+        if (board.boardCategory != BoardCategory.MODIFY) {
+            throw BadRequestException(ExceptionCode.MISMATCH_BOARD_TYPE)
         }
     }
 
-    private void isNotModifyBoard(Board board) {
-        if(board.getBoardCategory().equals(BoardCategory.MODIFY)) {
-            throw new BadRequestException(MISMATCH_BOARD_TYPE);
+    private fun isNotModifyBoard(board: Board) {
+        if (board.boardCategory == BoardCategory.MODIFY) {
+            throw BadRequestException(ExceptionCode.MISMATCH_BOARD_TYPE)
         }
     }
 }
