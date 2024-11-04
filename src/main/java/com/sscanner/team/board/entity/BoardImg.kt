3c5c881601a6c7af9477181b1733309f4650de8f
ihun-il <hunil9978@gmail.com>
2024-11-04 12:24:07 +0900

    migration: BoardImg Entity 마이그레이션

--- src/main/java/com/sscanner/team/board/entity/BoardImg.kt
+++ src/main/java/com/sscanner/team/board/entity/BoardImg.kt
@@ -1,40 +1,36 @@
-package com.sscanner.team.board.entity;
+package com.sscanner.team.board.entity
 
-import com.sscanner.team.global.common.BaseEntity;
-import jakarta.persistence.*;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import org.hibernate.annotations.SQLRestriction;
+import com.sscanner.team.global.common.BaseEntity
+import jakarta.persistence.*
+import org.hibernate.annotations.SQLRestriction
 
-import static lombok.AccessLevel.PROTECTED;
-
-@Getter
 @Entity
 @Table(name = "Board_img")
-@NoArgsConstructor(access = PROTECTED)
 @SQLRestriction("deleted_at IS NULL")
-public class BoardImg extends BaseEntity {
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+class BoardImg(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "board_img_id", nullable = false)
-    private Long id;
+    val id: Long? = null,
 
     @Column(name = "board_id", nullable = false)
-    private Long boardId;
+    var boardId: Long,
 
     @Column(name = "board_img_url", nullable = false)
-    private String boardImgUrl;
+    var boardImgUrl: String
+) : BaseEntity() {
 
-    @Builder
-    public BoardImg(Long boardId, String boardImgUrl) {
-        this.boardId = boardId;
-        this.boardImgUrl = boardImgUrl;
-    }
+    constructor() : this(
+        boardId = 0L,
+        boardImgUrl = ""
+    )
 
-    public static BoardImg makeBoardImg(Long boardId, String boardImgUrl) {
-        return BoardImg.builder()
-                .boardId(boardId)
-                .boardImgUrl(boardImgUrl)
-                .build();
+    companion object {
+        fun create(boardId: Long, boardImgUrl: String): BoardImg {
+            return BoardImg(
+                boardId = boardId,
+                boardImgUrl = boardImgUrl
+            )
+        }
     }
 }
