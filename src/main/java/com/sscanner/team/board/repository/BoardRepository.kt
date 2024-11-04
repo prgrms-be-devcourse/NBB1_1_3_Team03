bd86340660dc05e7d53424fa5ab4413b03a2c66a
ihun-il <hunil9978@gmail.com>
2024-11-04 12:23:31 +0900

    migration: BoardRepository 마이그레이션

--- src/main/java/com/sscanner/team/board/repository/BoardRepository.kt
+++ src/main/java/com/sscanner/team/board/repository/BoardRepository.kt
@@ -1,24 +1,28 @@
-package com.sscanner.team.board.repository;
+package com.sscanner.team.board.repository
 
-import com.sscanner.team.board.entity.Board;
-import com.sscanner.team.board.type.ApprovalStatus;
-import com.sscanner.team.board.type.BoardCategory;
-import com.sscanner.team.trashcan.type.TrashCategory;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
+import com.sscanner.team.board.entity.Board
+import com.sscanner.team.board.type.ApprovalStatus
+import com.sscanner.team.board.type.BoardCategory
+import com.sscanner.team.trashcan.type.TrashCategory
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
 
-public interface BoardRepository extends JpaRepository<Board, Long> {
+interface BoardRepository : JpaRepository<Board, Long> {
     @Query("select b from Board b where b.boardCategory = :boardCategory and b.trashCategory = :trashCategory")
-    Page<Board> findAllByCategories(@Param("boardCategory") BoardCategory boardCategory,
-                                                       @Param("trashCategory")TrashCategory trashCategory,
-                                                       Pageable pageable);
+    fun findAllByCategories(
+        @Param("boardCategory") boardCategory: BoardCategory,
+        @Param("trashCategory") trashCategory: TrashCategory,
+        pageable: Pageable
+    ): Page<Board>
 
     @Query("select b from Board b where b.approvalStatus = :approvalStatus and b.boardCategory = :boardCategory and b.trashCategory = :trashCategory")
-    Page<Board> findAllByStatusAndCategories(@Param("approvalStatus") ApprovalStatus approvalStatus,
-                                             @Param("boardCategory") BoardCategory boardCategory,
-                                             @Param("trashCategory") TrashCategory trashCategory,
-                                             Pageable pageable);
+    fun findAllByStatusAndCategories(
+        @Param("approvalStatus") approvalStatus: ApprovalStatus,
+        @Param("boardCategory") boardCategory: BoardCategory,
+        @Param("trashCategory") trashCategory: TrashCategory,
+        pageable: Pageable
+    ): Page<Board>
 }
