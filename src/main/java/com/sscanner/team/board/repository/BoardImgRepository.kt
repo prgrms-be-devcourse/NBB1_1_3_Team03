e8fd6e98522e0fa75adee6551183fb89522fedd9
ihun-il <hunil9978@gmail.com>
2024-11-04 12:24:24 +0900

    migration: BoardImg Repository 마이그레이션

--- src/main/java/com/sscanner/team/board/repository/BoardImgRepository.kt
+++ src/main/java/com/sscanner/team/board/repository/BoardImgRepository.kt
@@ -1,19 +1,16 @@
-package com.sscanner.team.board.repository;
+package com.sscanner.team.board.repository
 
-import com.sscanner.team.board.entity.BoardImg;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
+import com.sscanner.team.board.entity.BoardImg
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Modifying
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
 
-import java.util.List;
-
-public interface BoardImgRepository extends JpaRepository<BoardImg, Long> {
-    List<BoardImg> findAllByBoardId(Long boardId);
+interface BoardImgRepository : JpaRepository<BoardImg, Long> {
+    fun findAllByBoardId(boardId: Long): List<BoardImg>
+    fun existsByBoardIdAndAndBoardImgUrl(boardId: Long, imgUrl: String): Boolean
 
     @Modifying
     @Query("update BoardImg bi set bi.deletedAt = NOW() where bi.boardId = :boardId")
-    void deleteAll(@Param("boardId") Long boardId);
-
-    boolean existsByBoardIdAndAndBoardImgUrl(Long boardId, String imgUrl);
+    fun deleteAll(@Param("boardId") boardId: Long)
 }
