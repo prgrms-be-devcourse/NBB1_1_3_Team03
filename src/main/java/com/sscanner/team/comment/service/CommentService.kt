1d96d3b2ac4fe3919718f225548ee82eed927e0f
ihun-il <hunil9978@gmail.com>
2024-11-04 14:16:11 +0900

    migration: Comment Service 마이그레이션

--- src/main/java/com/sscanner/team/comment/service/CommentService.kt
+++ src/main/java/com/sscanner/team/comment/service/CommentService.kt
@@ -1,13 +1,11 @@
-package com.sscanner.team.comment.service;
+package com.sscanner.team.comment.service
 
-import com.sscanner.team.comment.requestdto.CommentCreateRequestDTO;
-import com.sscanner.team.comment.responsedto.CommentResponseDTO;
+import com.sscanner.team.comment.requestdto.CommentCreateRequestDTO
+import com.sscanner.team.comment.responsedto.CommentResponseDTO
 
-import java.util.List;
-
-public interface CommentService {
-    CommentResponseDTO saveComment(CommentCreateRequestDTO commentCreateRequestDTO);
-    void deleteComment(Long commentId);
-    List<CommentResponseDTO> getComments(Long boardId);
-    void deleteAll(Long boardId);
+interface CommentService {
+    fun saveComment(commentCreateRequestDTO: CommentCreateRequestDTO): CommentResponseDTO
+    fun deleteComment(commentId: Long)
+    fun getComments(boardId: Long): List<CommentResponseDTO>
+    fun deleteAll(boardId: Long)
 }
--- src/main/java/com/sscanner/team/comment/service/CommentServiceImpl.kt
+++ src/main/java/com/sscanner/team/comment/service/CommentServiceImpl.kt
@@ -1,80 +1,76 @@
-package com.sscanner.team.comment.service;
+package com.sscanner.team.comment.service
 
-import com.sscanner.team.user.entity.User;
-import com.sscanner.team.board.service.BoardService;
-import com.sscanner.team.comment.entity.Comment;
-import com.sscanner.team.comment.repository.CommentRepository;
-import com.sscanner.team.comment.requestdto.CommentCreateRequestDTO;
-import com.sscanner.team.comment.responsedto.CommentResponseDTO;
-import com.sscanner.team.global.exception.BadRequestException;
-import com.sscanner.team.global.utils.UserUtils;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
-import java.util.stream.Collectors;
-
-import static com.sscanner.team.global.exception.ExceptionCode.*;
+import com.sscanner.team.board.service.BoardService
+import com.sscanner.team.comment.entity.Comment
+import com.sscanner.team.comment.repository.CommentRepository
+import com.sscanner.team.comment.requestdto.CommentCreateRequestDTO
+import com.sscanner.team.comment.responsedto.CommentResponseDTO
+import com.sscanner.team.comment.responsedto.CommentResponseDTO.Companion.from
+import com.sscanner.team.global.exception.BadRequestException
+import com.sscanner.team.global.exception.ExceptionCode
+import com.sscanner.team.global.utils.UserUtils
+import com.sscanner.team.user.entity.User
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+import java.util.stream.Collectors
 
 @Service
-@RequiredArgsConstructor
 @Transactional(readOnly = true)
-public class CommentServiceImpl implements CommentService{
+class CommentServiceImpl(
+    private val commentRepository: CommentRepository,
+    private val userUtils: UserUtils,
+    private val boardService: BoardService
+) : CommentService {
 
-    private final CommentRepository commentRepository;
-    private final UserUtils userUtils;
-    private final BoardService boardService;
-
-    @Override
     @Transactional
-    public CommentResponseDTO saveComment(CommentCreateRequestDTO commentCreateRequestDTO) {
-        boardService.getBoard(commentCreateRequestDTO.boardId());
+    override fun saveComment(commentCreateRequestDTO: CommentCreateRequestDTO): CommentResponseDTO {
+        boardService.getBoard(commentCreateRequestDTO.boardId!!)
 
-        User user = userUtils.getUser();
+        val user = userUtils.user
 
-        Comment comment = commentCreateRequestDTO.toEntityComment(user);
+        val comment = commentCreateRequestDTO.toEntityComment(user)
 
-        commentRepository.save(comment);
+        commentRepository.save(comment)
 
-        return CommentResponseDTO.from(comment);
+        return from(comment)
     }
 
-    @Override
     @Transactional
-    public void deleteComment(Long commentId) {
-        User user = userUtils.getUser();
-        Comment comment = getComment(commentId);
+    override fun deleteComment(commentId: Long) {
+        val user = userUtils.user
+        val comment = getComment(commentId)
 
-        isMatchAuthor(user, comment);
+        isMatchAuthor(user, comment)
 
-        commentRepository.delete(comment);
+        commentRepository.delete(comment)
     }
 
-    @Override
-    public List<CommentResponseDTO> getComments(Long boardId) {
-        List<Comment> comments = commentRepository.findAllByBoardId(boardId);
+    override fun getComments(boardId: Long): List<CommentResponseDTO> {
+        val comments = commentRepository.findAllByBoardId(boardId)
 
-        return comments.stream()
-                .map(comment -> CommentResponseDTO.from(comment))
-                .collect(Collectors.toList());
+        return comments!!.stream()
+            .map { comment: Comment? ->
+                from(
+                    comment!!
+                )
+            }
+            .collect(Collectors.toList())
     }
 
-    @Override
     @Transactional
-    public void deleteAll(Long boardId) {
-        commentRepository.deleteAllByBoardId(boardId);
+    override fun deleteAll(boardId: Long) {
+        commentRepository.deleteAllByBoardId(boardId)
     }
 
-    private Comment getComment(Long commentId) {
+    private fun getComment(commentId: Long): Comment {
         return commentRepository
-                .findById(commentId)
-                .orElseThrow(() -> new BadRequestException(NOT_EXIST_COMMENT));
+            .findById(commentId)
+            .orElseThrow { BadRequestException(ExceptionCode.NOT_EXIST_COMMENT) }!!
     }
 
-    private void isMatchAuthor(User user, Comment comment) {
-        if(!comment.getUser().equals(user)) {
-            throw new BadRequestException(MISMATCH_AUTHOR);
+    private fun isMatchAuthor(user: User, comment: Comment) {
+        if (comment.user != user) {
+            throw BadRequestException(ExceptionCode.MISMATCH_AUTHOR)
         }
     }
 }
